e252e878912f63fd9c2df62467e5e2a73b665865
Froilan Lugue <froilan.lugue@mvp.net.ph>
2021-11-09 22:29:54 +0800

    Added List and Titles component . Added material and material/icons

---
+++ Trello.Web/ClientApp/src/components/List/List.tsx
@@ -0,0 +1,26 @@
+﻿import React from 'react';
+import { CssBaseline, makeStyles, Paper, Typography } from '@material-ui/core'
+import Title from './Title';
+
+const useStyle = makeStyles((theme)=>({
+    root: {
+        width: '300px',
+        backgroundColor: '#ebecf0',
+        marginLeft: theme.spacing(1)
+    }
+}));
+
+const List = () => {
+    const classes = useStyle();
+
+    return (
+        <div>
+            <Paper className={classes.root}>
+                <CssBaseline />
+                <Title/>
+            </Paper>
+        </div>
+    );
+}
+
+export default List;
---
+++ Trello.Web/ClientApp/src/components/List/Title.tsx
@@ -0,0 +1,51 @@
+﻿import { InputBase, makeStyles, Typography } from '@material-ui/core';
+import  MoreHoriz from '@material-ui/icons/MoreHoriz';
+import React, { useState } from 'react';
+
+const useStyle = makeStyles((theme) => ({
+    editableTitleContainer: {
+        margin: theme.spacing(1),
+        display: 'flex',
+    },
+    editableTitle: {
+        flexGrow: 1,
+    },
+    input: {
+        margin: theme.spacing(1),
+        '&:focus': {
+            backgroundColor: '#ddd'
+        }
+    }
+}));
+
+const Title = () => {
+    const [open, setOpen] = useState(false);
+    const classes = useStyle();
+
+    return (
+        <div>
+            {
+                open ? (
+                    <div>
+                        <InputBase value='Todo'
+                            inputProps={{ className: classes.input }}
+                            fullWidth
+                            onBlur={() => setOpen(!open)}
+                        />
+                    </div >
+                ) : (
+                    <div className={classes.editableTitleContainer}>
+                        <Typography
+                            onClick={() => setOpen(!open)}
+                            className={classes.editableTitle}
+                        >
+                            Todo
+                        </Typography>
+                        <MoreHoriz />
+                    </div>
+                )}
+        </div>
+    );
+}
+
+export default Title;
